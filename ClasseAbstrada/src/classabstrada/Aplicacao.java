2b8ee83d1126867099c10c5ddbf4ea76b0fdccc5
Fantunesds <fantunesds@gmail.com>
2021-07-06 18:55:24 -0300

    fantunew

---
+++ ClasseAbstrada/src/classabstrada/Aplicacao.java
@@ -0,0 +1,10 @@
+package classabstrada;
+
+public class Aplicacao {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
---
+++ ClasseAbstrada/src/classabstrada/Item.java
@@ -0,0 +1,11 @@
+package classabstrada;
+
+public abstract class Item {
+	
+	private int posX;
+	private int posY;
+	
+	public void pegar() {
+		
+	}
+}
